5832fb69bbdbd0e3af0656428d4950d52af99fee
Tudor Golubenco <tudor@packetbeat.com>
2015-08-04 17:13:54 +0200

    Fixed user name

--- docker/build_latest.sh
+++ docker/build_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t tudor_g/xgo-base base/ && \
-    docker build -t tudor_g/xgo-1.4.2 go-1.4.2/ &&
-    docker build -t tudor_g/xgo-1.4.x go-1.4.x/ &&
-    docker build -t tudor_g/xgo-latest go-latest/
+docker build -t tudorg/xgo-base base/ && \
+    docker build -t tudorg/xgo-1.4.2 go-1.4.2/ &&
+    docker build -t tudorg/xgo-1.4.x go-1.4.x/ &&
+    docker build -t tudorg/xgo-latest go-latest/
