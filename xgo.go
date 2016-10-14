c9f39ddf22d3d4c4301fe3e226e6e2bd4a9b5ea6
Nguoi An Phu <nguoianphu@gmail.com>
2016-10-14 15:35:48 +0700

    Use local images

--- xgo.go
+++ xgo.go
@@ -39,8 +39,8 @@ func init() {
 }
 
 // Cross compilation docker containers
-var dockerBase = "karalabe/xgo-base"
-var dockerDist = "karalabe/xgo-"
+var dockerBase = "xgo-base"
+var dockerDist = "xgo-"
 
 // Command line arguments to fine tune the compilation
 var (
